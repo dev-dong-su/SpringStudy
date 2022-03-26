af3d3b6250159f107333f3c92ab0f54cd61a9bb0
Dong_su <fkdlrj1230@gmail.com>
2022-03-26 22:33:34 +0900

    Initial commit

---
+++ src/main/java/com/dongsu/book/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.dongsu.book.web;
+
+import com.dongsu.book.web.dto.HelloResponseDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
